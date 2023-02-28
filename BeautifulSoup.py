c2209991718661b5f4c12ea4d64c08b4a6834863
Emerson Viana <missonzito@outlook.com>
2023-02-28 19:37:54 -0300

    BeatifulSoup, html, lxml, html5lib

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 
 html = urlopen('http://www.pythonscraping.com/pages/page1.html')
@@ -19,3 +20,22 @@ except HTTPError as e:
 else:
     # o programa continua. Nota: se você retornar ou executar um break no
     # catch da exceção, não será necessária a instrução "else"
+
+    try:
+        html = urlopen('http://pythonscrapingthisurldoesnotexist.com')
+    except HTTPError as e:
+        print(e)
+    except URLError as e:
+        print('The server could not be found!')
+    else:
+        print('It Worked!')
+        print(bs.nonExistentTag)
+        print(bs.nonExistentTag.someTag)
+        
+        try:
+            badContent = bs.nonExistentTag.anotherTag
+        except AttributeError as e:
+            print('Tag was not found')
+        else:
+            if badContent == None:
+                print(badContent)
